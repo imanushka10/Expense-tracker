3bbac91617cf2679f71501637676d2a983dad3f1
Anu Singh <anusingh1883656@gmail.com>
2020-12-28 11:58:32 +0530

    tracker components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from "@material-ui/core"
+import useStyles from "./styles"
+
+
+const Main = () => {
+    const classes = useStyles()
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="POwered by speechly"></CardHeader>
+            <CardContent>
+                <Typography align="center" variant="h5">
+                    Total Balance $100
+                </Typography>
+                <Typography variant="subtitle1" style={{ lineHeight: "1.5rem", marginTop: "20px" }} >
+                    {/* infocard container */}
+
+                    Try saving: Add income for $100 in category salary for Monday.....
+                </Typography>
+                <Divider></Divider>
+                {/* Form */}
+
+            </CardContent>
+            <CardContent className={classes.cartContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        {/* List */}
+
+                    </Grid>
+                </Grid>
+
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
